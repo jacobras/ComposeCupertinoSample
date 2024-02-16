895510a468996ac344e1211806580c040d55cdb1
Jacob Ras <jacobras.nl@gmail.com>
2024-02-16 15:48:15 +0100

    Make dark theme work in Material

--- composeApp/src/commonMain/kotlin/AppTheme.kt
+++ composeApp/src/commonMain/kotlin/AppTheme.kt
@@ -1,11 +1,20 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AppTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
+        colorScheme = if (useDarkTheme) {
+            darkColorScheme()
+        } else {
+            lightColorScheme()
+        },
         content = content
     )
 }
